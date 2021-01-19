30bcd9b51baf2506413cfc8c6f705ad475116c38
Hugo Holmqvist <hugo.holm@live.com>
2021-01-19 21:36:44 +0200

    Add meta tags for seo

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -5,6 +5,8 @@ import AreasPieChart from '../components/AreasPieChart'
 import Footer from '../components/Footer'
 import { getAdministrations, getSummary } from '../service/piikki'
 import DailyStatsChart from '../components/DailyStatsChart'
+import Head from 'next/head'
+
 
 type AppProps = {
   totalVaccinatees: number
@@ -12,8 +14,20 @@ type AppProps = {
   administrations: AreaAdministration[]
 }
 
+const head =
+  <Head>
+    <title>COVID-19 vaccination status in Finland</title>
+    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    <meta httpEquiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta name="description" content="View charts of the current COVID-19 vaccination status in Finland."/>
+    <meta property="og:title" content="COVID-19 vaccination status in Finland" />
+    <meta property="og:description" content="View charts of the current COVID-19 vaccination status in Finland." />
+    <meta property="og:type" content="website" />
+  </Head>
+
 export default ({ summary, totalVaccinatees, administrations }: AppProps) => (
   <>
+    {head}
     <div className="container">
       <h1>COVID-19 vaccinations in 🇫🇮</h1>
       <AdministrationPieChart summary={summary} totalVaccinatees={totalVaccinatees} />
