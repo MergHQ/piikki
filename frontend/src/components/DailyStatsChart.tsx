31c2a5b44cd7e7875de0c9c4a55b920c61cd99cf
Hugo Holmqvist <hugo.holm@live.com>
2021-02-01 22:23:12 +0200

    Filter chart to show only one week at a time

--- frontend/src/components/DailyStatsChart.tsx
+++ frontend/src/components/DailyStatsChart.tsx
@@ -2,13 +2,19 @@ import { h } from 'harmaja'
 import { AreaAdministration } from '../../../shared/area-administration'
 import { ChartData } from 'chart.js'
 import { getWithIdx } from './chartColors'
-import { startOfDay, format } from 'date-fns'
+import { startOfDay, format, startOfWeek, sub, add, isSameWeek, isAfter } from 'date-fns'
 import * as R from 'ramda'
 import { Bar } from '../util/chart-js-wrapper'
 import * as L from 'lonna'
+import './week-selector.css'
 
 type Props = {
   administrations: L.Property<AreaAdministration[]>
+}
+
+type Combined = {
+  administrations: AreaAdministration[]
+  selectedWeek: Date
 }
 
 const options = {
@@ -47,10 +53,68 @@ const toChartData = (administrations: AreaAdministration[]): ChartData => {
   }
 }
 
+const applyWeekFilter = ({
+  administrations,
+  selectedWeek,
+}: Combined): AreaAdministration[] =>
+  administrations.map(({ shotHistory, ...rest }) => ({
+    ...rest,
+    shotHistory: shotHistory.filter(({ date }) =>
+      isSameWeek(selectedWeek, new Date(date))
+    ),
+  }))
+
+const isWeekDisabled = (d: Date) => isAfter(d, startOfWeek(new Date()))
+
+const selectedWeek = L.atom(startOfWeek(new Date()))
+const disableNextWeek = selectedWeek.pipe(
+  L.map<Date, boolean>(d => isWeekDisabled(add(d, { weeks: 1 })))
+)
+
+const handleWeekChange = (op: 'add' | 'sub') => (_: unknown) => {
+  switch (op) {
+    case 'add':
+      const selected = selectedWeek.get()
+      const nextWeek = add(selected, { weeks: 1 })
+      if (!isWeekDisabled(nextWeek)) {
+        selectedWeek.set(nextWeek)
+      }
+      break
+    case 'sub':
+      selectedWeek.modify(d => sub(d, { weeks: 1 }))
+      break
+    default:
+      console.warn('Unkown op', op)
+  }
+}
+
+const WeekSelector = () => (
+  <div className="week-selector-controls">
+    <button className="week-selector__button" onClick={handleWeekChange('sub')}>
+      ᐊ
+    </button>
+    <p>{L.view(selectedWeek, ts => `Week ${format(ts, 'w yyyy')}`)}</p>
+    {L.view(disableNextWeek, disabled => (
+      <button
+        className="week-selector__button"
+        disabled={disabled}
+        onClick={handleWeekChange('add')}
+      >
+        ᐅ
+      </button>
+    ))}
+  </div>
+)
+
 export default ({ administrations }: Props) => (
   <div className="data-container">
     <h2 className="data-container__title">Daily vaccinations administered per area</h2>
-    {administrations.pipe(
+    <WeekSelector />
+    {L.combineTemplate({
+      administrations,
+      selectedWeek,
+    }).pipe(
+      L.map<Combined, AreaAdministration[]>(applyWeekFilter),
       L.map(toChartData),
       L.map(data => <Bar data={data} options={options} />)
     )}
